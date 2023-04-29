fabe8047be6c23a82f5723d4c649f6743e91a8aa
Oscar Castillejo <oacs1221@gmail.com>
2023-04-29 14:49:22 -0400

    feat(nvim): add transparent nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -333,6 +333,24 @@ require("lazy").setup({
 			require("codegpt.config")
 		end,
 	},
+
+	{
+		"xiyaowong/transparent.nvim",
+		config = function ()
+			require("transparent").setup({
+				groups = { -- table: default groups
+					'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+					'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+					'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+					'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+				},
+				extra_groups = {
+					"NvimTreeNormal" -- NvimTree
+				}, -- table: additional groups that should be cleared
+				exclude_groups = {}, -- table: groups you don't want to clear
+			})
+		end
+	}
 })
 
 -- [[ Setting options ]]
