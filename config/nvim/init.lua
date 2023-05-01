75dc9b9207dacbc9ca4bc8318a6532dde8cf5cd2
Oscar Castillejo <oacs1221@gmail.com>
2023-05-01 12:28:24 -0400

    feat(nvim): change neoformat

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -127,36 +127,42 @@ require("lazy").setup({
 	{
 		"sbdchd/neoformat",
 		config = function()
-			g.neoformat_enabled_vue = { "prettierd", "eslint_d" }
-			g.neoformat_enabled_javascript = { "prettierd", "eslint_d" }
-			g.neoformat_enabled_typescript = { "prettier", "eslint" }
-			g.neoformat_enabled_json = { "prettierd" }
-			g.neoformat_enabled_go = { "gofmt" }
-			g.neoformat_enabled_lua = { "stylua" }
-			g.neoformat_enabled_vim = { "prettier" }
-			g.neoformat_enabled_prisma = { "prettier" }
-			g.neoformat_enabled_shell = { "shmft", "prettierd" }
-			g.neoformat_enabled_sh = { "shmft", "prettierd" }
-			g.neoformat_run_all_formatters = 1
-			g.shfmt_opt = "ci"
-			g.neoformat_basic_format_align = 1
-			g.neoformat_basic_format_retab = 1
-			g.neoformat_basic_format_trim = 1
+			-- g.neoformat_enabled_vue = { "prettierd", "eslint_d" }
+			-- g.neoformat_enabled_javascript = { "prettierd", "eslint_d" }
+			-- g.neoformat_enabled_typescript = { "prettier", "eslint" }
+			-- g.neoformat_enabled_json = { "prettierd" }
+			-- g.neoformat_enabled_go = { "gofmt" }
+			-- g.neoformat_enabled_lua = { "stylua" }
+			-- g.neoformat_enabled_vim = { "prettier" }
+			-- g.neoformat_enabled_prisma = { "prettier" }
+			-- g.neoformat_enabled_shell = { "shmft", "prettierd" }
+			-- g.neoformat_enabled_sh = { "shmft", "prettierd" }
+			-- g.neoformat_run_all_formatters = 1
+			-- g.shfmt_opt = "ci"
+			-- g.neoformat_basic_format_align = 1
+			-- g.neoformat_basic_format_retab = 1
+			-- g.neoformat_basic_format_trim = 1
 
 			vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 				pattern = {
-					"*.rs",
-					"*.astro",
 					"*.lua",
-					"*.sh",
+				},
+				command = "Neoformat stylua",
+			})
+			vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+				pattern = {
 					"*.ts",
 					"*.vue",
 					"*.json",
 					"*.tsx",
 					"*.js",
+					"*.jsx",
+					"*.css",
+					"*.scss",
+					"*.sass",
 					"*.html",
 				},
-				command = "undojoin | Neoformat",
+				command = "Neoformat prettier",
 			})
 		end,
 	},
